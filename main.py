ef877b24ccd703206a2863b792c1167d1177db82
rezik <rezikun@gmail.com>
2021-03-24 21:12:53 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import telebot
+
+bot = telebot.TeleBot('1795844570:AAFeqFHRpWd07HiS0SUmwe4JmL7z2-gGXOI')
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+
+bot.polling(none_stop=True)
